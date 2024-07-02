856783cd4341862d0d4f3d8f51a11d6aeb458c75
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-07-02 17:15:58 +0530

    Time: 7 ms (28.33%), Space: 11.1 MB (34.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,9 +3,9 @@ public:
     string longestCommonPrefix(vector<string>& str) {
         sort(str.begin(),str.end());
         int n = str.size();
-        string ans ;
-        int i =0;
-        int j =0;
+        int i=0;
+        int j=0;
+        string ans = "";
         while(i<str[0].size() and j<str[n-1].size()){
             if(str[0][i]!=str[n-1][j]){
                 break;
@@ -13,7 +13,7 @@ public:
             ans+=str[0][i];
             i++;
             j++;
-        }   
+        }
         return ans;
     }
 };
