6fb50984c3ba3eec4bc4de6466d736f40b635e5f
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-07-02 22:15:35 +0530

    Time: 3 ms (58.19%), Space: 8.4 MB (21.98%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,26 @@
 class Solution {
-    public :
+public:
     int myAtoi(string s) {
-        if(s.length()==0) return 0;
-
+        if(s.size()==0){
+            return 0;
+        }
         int i=0;
-         while(i<s.size() && s[i]== ' ') {
-             i++;
-         }     
-        s = s.substr(i); //i ---> last of string
-        
-        int sign = +1;
-        long ans = 0;
-        
-        if(s[0] == '-') sign = -1;
-        
-        int MAX = INT_MAX, MIN = INT_MIN;
-        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s[0] == ' ' || !isdigit(s[i])) break;
-            
-            ans = ans * 10 + s[i]-'0';
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
+        while(i<s.size() and s[i]==' '){
             i++;
         }
-        
+        s = s.substr(i);
+        int sign = 1;
+        long ans = 0;
+        if(s[0]=='-')sign = -1;
+        int MAX = INT_MAX , MIN = INT_MIN;
+        i = (s[0]=='+' || s[0]=='-') ? 1:0;
+        while(i<s.size()){
+            if(s[0]==' '|| !isdigit(s[i])) break;
+            ans = ans*10+s[i]-'0';
+            if(sign == -1 and -1 *ans <MIN) return MIN;
+            if(sign ==  1 and ans> MAX) return MAX;
+            i++;
+        }
         return (int)(sign*ans);
     }
 };
